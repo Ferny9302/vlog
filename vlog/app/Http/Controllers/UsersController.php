b5f52e1908b0d35debfc50f693c7c8764f3b5cd1
Ferny9302 <111541650+Ferny9302@users.noreply.github.com>
2025-10-08 15:21:01 -0700

    agregar posts

--- vlog/app/Http/Controllers/UsersController.php
+++ vlog/app/Http/Controllers/UsersController.php
@@ -8,6 +8,9 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
     public function getUsers(){
         /*Selec * from users*/
         $data = User::all();
